7f01218ecd50910731b14153666917309a66de76
Gary HL <manar.benkraouda@gmail.com>
2023-06-17 11:18:54 +0200

    created eventsCarousel

--- frontend/src/components/servicesSection/ecoEventsCarousel/ecoEventsCarousel.jsx
+++ frontend/src/components/servicesSection/ecoEventsCarousel/ecoEventsCarousel.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './ecoEventsCarousel.css';
+import dataEvents from '../../../utilities/dataEvents.json'
 
 export function EcoEventsCarousel(props) {
   const format = props.format
@@ -17,50 +18,35 @@ export function EcoEventsCarousel(props) {
   }
 
   useEffect(() => {
-    const getEvents = async () => {
-        try {
-            const headers = {
-                Authorization: 'Bearer J65QQVUTS5O6WFYFRI3A',
-            };
-            const response = await fetch('https://www.eventbrite.es/api/v3/destination/events/?event_ids=657437993737,652541448037,464342449767,635964275267,653926651217,600166232357,653161031227,600462508527,656741570717,638005109457,650671976397,652268010177,651116877107,652637254597&page_size=14&expand=event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections', { headers });
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-                setEvents(data.events);
-            } else {
-                console.error('Error:', response.status);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+    setEvents(dataEvents.events.slice(1, 6))
+  }, []);
+  const handleButtonClick = (url) => {
+    window.open(url, '_blank');
+  };
 
-    getEvents();
-}, []);
 
-const testData = ['hola', 'honlas', 'adsfasdfasfd'];
-
-return (
+  return (
     <div>
-    <div className="circlecarousel" />
-    <h2>Haz match con gente como tú</h2>
-    <div className="container">
+      <h2 className="title_section_carousel">Haz <span className='accent_title'>match</span> con gente como tú</h2>
       {events.length === 0 ? (
         <p>Loading events...</p>
       ) : (
-        <div className="cardContainer">
+        <div className="container">
           {events.map((event) => (
-            <div className="card" key={event.id}>
-              <img alt={event.name.text} src={event.logo.url} className="image_card" />
-              <h3>{event.name.text}</h3>
-              <p>Date: {event.start.local}</p>
-              <p>Location: {event.venue.address}</p>
-              <button>Make Appointment</button>
+            <div className="card" key={event?.id}>
+              <img alt={event?.name.text} src={event.image.original.url} className="image_card" />
+              <div className="card_details">
+                <h3>{event?.name?.length > 25 ? event.name.slice(0,40)+'...' : event?.name }</h3>
+                <p className='card_date'>Date: {event.start_date} </p>
+                <p className='card_location'>{event?.primary_venue?.address?.address_1}</p>
+                <button className='button_card_carousel' onClick={() => handleButtonClick(event?.url)}>Join</button>
+              </div>
             </div>
           ))}
         </div>
       )}
+      <div className="circlecarousel" />
+
     </div>
-  </div>
-);
+  );
 }
